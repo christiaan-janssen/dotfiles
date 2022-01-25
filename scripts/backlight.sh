ac96f6ed749a44e921fca6d3e6a23dc74fdcffac
christiaan-janssen <christiaanjanssen@outlook.com>
2022-01-25 12:15:52 +0100

    Add misc items

---
+++ scripts/backlight.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+list=()
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    line=$(cd $HOME/projects/dotfiles/scripts/ && ./brightness.sh | grep $m)
+    cut_lin=$(echo $line | grep -o -P 'brightness: .{0,4}')
+    list+=( "$m:${cut_lin:12:3}" )
+done
+
+printf " %s " "${list[@]}"
