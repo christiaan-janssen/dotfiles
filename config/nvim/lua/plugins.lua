86297f2750af26fbc74e3a8ffb948c560d7f62c9
Christiaan Janssen <christiaanjanssen@outlook.com>
2023-05-06 09:47:35 +0200

    Add tmux plugin for nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -33,6 +33,8 @@ return require('packer').startup(function()
     end
   }
 
+  use { 'christoomey/vim-tmux-navigator' }
+
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
