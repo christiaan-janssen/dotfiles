517bd9cf16e4b6740e3eb1e73ce82fdef4ffc9a4
christiaan-janssen <christiaanjanssen@outlook.com>
2024-06-30 11:42:37 +0200

    chore: Move config from stow to tuckr

--- nixos/users.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-{
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.christiaan = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  };
-}
